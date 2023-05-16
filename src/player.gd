55ef7efe67d938e6973d44244d229fee59866bef
GrybasBaravykas <danielius.vitk@gmail.com>
2023-05-16 16:46:42 +0300

    mapo generacija

--- src/player.gd
+++ src/player.gd
@@ -4,6 +4,14 @@ var gravity = 30
 const jump_impulse = 500
 var Wall = preload("res://src/walls.tscn")
 
+const Json = preload("res://map_gen_json.gd")
+var my_json: Json
+
+
+func _ready():
+	my_json = Json.new()
+	my_json.load_file()
+
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = 0
@@ -12,9 +20,11 @@ func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta)
 
 func Wall_reset():  
-	var Wall_instance =Wall.instantiate()
-	Wall_instance.position = Vector2(1300,randi_range(150,460))
-	get_parent().call_deferred("add_child",Wall_instance)
+	var Wall_instance = Wall.instantiate()
+	var wall_pos = my_json.take_gen_val()
+	print(wall_pos)
+	Wall_instance.position = Vector2(1300,randi_range(150 + wall_pos * 20, 150 + wall_pos * 20))
+	get_parent().call_deferred("add_child",Wall_instance) 
 
 
 func _on_resseter_body_entered(body: Node2D) -> void:
